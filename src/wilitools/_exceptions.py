62678128ae364e8f6bfb29909914c8d05d291e70
MaruKazeMaru <kaze47111@gmail.com>
2024-01-26 23:58:56 +0900

    delete no need class

--- src/wilitools/_exceptions.py
+++ src/wilitools/_exceptions.py
@@ -1,13 +1,6 @@
 # SPDX-FileCopyrightText: 2024 ShinagwaKazemaru
 # SPDX-License-Identifier: MIT License
 
-class UnexistRecord(Exception):
-    def __init__(self, table:str, columns:list[str]=[], values:list=[]):
-        wheres = []
-        for c, v in zip(columns, values):
-            wheres.append("{}={}".format(c, v))
-        super().__init__("table " + table + " has no record where " + ", ".join(wheres))
-
 class NegativeFloor(Exception):
     def __init__(self, xmin:float, xmax:float, ymin:float, ymax:float) -> None:
         messages = []
